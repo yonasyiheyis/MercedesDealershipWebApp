db39dc3369fda537a1ee1ef6590a1bd166b181b4
yonasyiheyis <yoniamde@gmail.com>
2021-04-10 11:11:16 -0500

    admin route added

--- DealershipBackend/app.js
+++ DealershipBackend/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var adminRouter = require('./routes/admin');
 
 var app = express();
 
@@ -17,6 +18,7 @@ app.use(cookieParser());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
