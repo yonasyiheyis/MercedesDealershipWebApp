dfdde2a7cc164952071b006a79d87f1622525833
sammy <samueltesfai41@gmail.com>
2021-04-11 23:37:28 -0500

    test

--- DealershipApp/src/app/app.module.ts
+++ DealershipApp/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+
 //angular material modules
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { YouTubePlayerModule } from "@angular/youtube-player";
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
+
+
 
 import { AppComponent } from './app.component';
 import { AdminComponent } from './admin/admin.component';
@@ -20,19 +28,21 @@ import { HomeComponent } from './home/home.component';
 import { EditComponent } from './edit/edit.component';
 import { HeaderComponent } from './header.component';
 import { ViewinventoryComponent } from './viewinventory.component';
+import { FooterComponent } from './footer.component';
+
 
 
 @NgModule({
-  declarations: [AppComponent, AdminComponent, AddComponent, SignupComponent, LoginComponent,HomeComponent, EditComponent, HeaderComponent, ViewinventoryComponent],
+  declarations: [AppComponent, AdminComponent, AddComponent, SignupComponent, LoginComponent, HomeComponent, EditComponent, HeaderComponent, ViewinventoryComponent, FooterComponent],
   imports: [
     HttpClientModule,
-    RouterModule.forRoot([{path:'',component:HomeComponent},
-    {path:'login',component:LoginComponent},
-    {path:'signup',component:SignupComponent},
-    {path:'admin',component:AdminComponent},
-    {path:'add',component:AddComponent},
-    {path:'edit',component:EditComponent},
-  ]),
+    RouterModule.forRoot([{ path: '', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: SignupComponent },
+    { path: 'admin', component: AdminComponent },
+    { path: 'add', component: AddComponent },
+    { path: 'edit', component: EditComponent },
+    ]),
     BrowserModule,
     BrowserAnimationsModule,
     MatFormFieldModule,
@@ -41,6 +51,10 @@ import { ViewinventoryComponent } from './viewinventory.component';
     MatInputModule,
     MatIconModule,
     MatButtonModule,
+    YouTubePlayerModule,
+    MatToolbarModule,
+    MDBBootstrapModule.forRoot()
+    
   ],
   providers: [],
   bootstrap: [AppComponent],
