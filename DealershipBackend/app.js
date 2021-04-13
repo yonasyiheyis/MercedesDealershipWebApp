3453dada23a97651696f9ca9044f1fe43fcbe1da
sammy <samueltesfai41@gmail.com>
2021-04-12 22:53:00 -0500

    pss

--- DealershipBackend/app.js
+++ DealershipBackend/app.js
@@ -13,7 +13,7 @@ var adminRouter = require("./routes/admin");
 var authRouter = require("./routes/auth");
 
 
-const uri = "mongodb+srv://sami:mwaprojectsami@cluster0.jizzk.mongodb.net/Dealership?retryWrites=true&w=majority"
+const uri = ""
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 
